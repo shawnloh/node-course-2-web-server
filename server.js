107b393c0f28b343e62394ab7a0436992a703587
Shawn Loh <shawnloh1996@gmail.com>
2019-02-21 18:53:49 +0800

    Added project page

--- server.js
+++ server.js
@@ -46,6 +46,11 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects', {
+    pageTitle: 'Projects'
+  });
+});
 app.get('/bad', (req, res) => {
   res.send({ errorMessage: 'Error found!' });
 });
