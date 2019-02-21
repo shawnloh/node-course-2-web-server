b44278fef686e5474bd5c1988e702cad74c339f4
Shawn Loh <shawnloh1996@gmail.com>
2019-02-21 17:44:25 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + 'public'));
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res, next) => {
+  res.render('home', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to Home'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({ errorMessage: 'Error found!' });
+});
+
+app.listen(3000, () => console.log('Server is online at port 3000'));
